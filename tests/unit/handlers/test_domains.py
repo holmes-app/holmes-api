41efc19b14a23b59fe87b0dfba0077df2030e1e9
Evandro Flores <eof@eof.com.br>
2013-10-26 01:36:33 -0200

    increase test coverage

--- tests/unit/handlers/test_domains.py
+++ tests/unit/handlers/test_domains.py
@@ -6,6 +6,7 @@ from ujson import loads
 from preggy import expect
 from tornado.testing import gen_test
 
+from holmes.models import Domain
 from tests.unit.base import ApiTestCase
 from tests.fixtures import DomainFactory
 
@@ -14,6 +15,7 @@ class TestDomainsHandler(ApiTestCase):
 
     @gen_test
     def test_can_save(self):
+        yield Domain.objects.delete()
         yield DomainFactory.create(url="http://globo.com", name="globo.com")
         yield DomainFactory.create(url="http://g1.globo.com", name="g1.globo.com")
 
@@ -36,3 +38,17 @@ class TestDomainsHandler(ApiTestCase):
         expect(domains[1]['url']).to_equal("http://globo.com")
         expect(domains[1]['violationCount']).to_equal(0)
         expect(domains[1]['pageCount']).to_equal(0)
+
+    @gen_test
+    def test_will_return_empty_list_when_no_domains(self):
+        yield Domain.objects.delete()
+
+        response = yield self.http_client.fetch(
+            self.get_url('/domains')
+        )
+
+        expect(response.code).to_equal(200)
+
+        domains = loads(response.body)
+
+        expect(domains).to_length(0)
