56007f50b392e8df000565bd303ee49584564444
Rafael Floriano da Silva <rflorianobr@gmail.com>
2013-11-20 21:55:16 -0200

    Ops

--- tests/unit/handlers/test_violation.py
+++ tests/unit/handlers/test_violation.py
@@ -13,42 +13,42 @@ from tests.unit.base import ApiTestCase
 from tests.fixtures import DomainFactory, PageFactory, ReviewFactory
 
 
-# class TestMostCommonViolationsHandler(ApiTestCase):
+class TestMostCommonViolationsHandler(ApiTestCase):
 
-#     @gen_test
-#     def test_can_get_most_common_violations(self):
-#         review = ReviewFactory.create()
-#         self.db.flush()
+    @gen_test
+    def test_can_get_most_common_violations(self):
+        review = ReviewFactory.create()
+        self.db.flush()
 
-#         url = self.get_url(
-#             '/page/%s/review/%s/violation' % (
-#                 review.page.uuid,
-#                 review.uuid
-#             )
-#         )
+        url = self.get_url(
+            '/page/%s/review/%s/violation' % (
+                review.page.uuid,
+                review.uuid
+            )
+        )
 
-#         response = yield self.http_client.fetch(
-#             url,
-#             method='POST',
-#             body='key=test.violation&title=title&description=description&points=20'
-#         )
+        response = yield self.http_client.fetch(
+            url,
+            method='POST',
+            body='key=test.violation&title=title&description=description&points=20'
+        )
 
-#         expect(response.code).to_equal(200)
-#         expect(response.body).to_equal('OK')
+        expect(response.code).to_equal(200)
+        expect(response.body).to_equal('OK')
 
-#         review = Review.by_uuid(review.uuid, self.db)
+        review = Review.by_uuid(review.uuid, self.db)
 
-#         expect(review).not_to_be_null()
-#         expect(review.violations).to_length(1)
+        expect(review).not_to_be_null()
+        expect(review.violations).to_length(1)
 
-          ## TODO: most common violations need to be tested without make a post to violation
+        # TODO: most common violations need to be tested without make a post to violation
 
-#         response = yield self.http_client.fetch(
-#             self.get_url('/most-common-violations')
-#         )
-#         expect(response.code).to_equal(200)
+        response = yield self.http_client.fetch(
+            self.get_url('/most-common-violations')
+        )
+        expect(response.code).to_equal(200)
 
-#         returned_json = loads(response.body)
-#         expect(returned_json).to_length(len(review.violations))
-#         expect(returned_json[0]['name']).to_equal('title')
-#         expect(returned_json[0]['count']).to_equal(1)
+        returned_json = loads(response.body)
+        expect(returned_json).to_length(len(review.violations))
+        expect(returned_json[0]['name']).to_equal('title')
+        expect(returned_json[0]['count']).to_equal(1)
--- tests/unit/models/test_worker.py
+++ tests/unit/models/test_worker.py
@@ -23,31 +23,31 @@ class TestWorker(ApiTestCase):
 
         expect(str(worker)).to_equal('Worker %s' % str(worker.uuid))
 
-    # def test_worker_current_review_page(self):
-    #     review = ReviewFactory.create()
-    #     worker = WorkerFactory.create(current_review=review)
+    def test_worker_current_review_page(self):
+        review = ReviewFactory.create()
+        worker = WorkerFactory.create(current_review=review)
 
-    #     loaded_worker = self.db.query(Worker).get(worker.id)
-    #     expect(loaded_worker.current_review.id).to_equal(review.id)
+        loaded_worker = self.db.query(Worker).get(worker.id)
+        expect(loaded_worker.current_review.id).to_equal(review.id)
 
-    # def test_worker_to_dict(self):
-    #     review = ReviewFactory.create()
-    #     worker = WorkerFactory.create(current_review=review)
+    def test_worker_to_dict(self):
+        review = ReviewFactory.create()
+        worker = WorkerFactory.create(current_review=review)
 
-    #     worker_dict = worker.to_dict()
+        worker_dict = worker.to_dict()
 
-    #     expect(worker_dict['uuid']).to_equal(str(worker.uuid))
-    #     expect(worker_dict['last_ping']).to_equal(str(worker.last_ping))
-    #     expect(worker_dict['current_review']).to_equal(str(review.uuid))
-    #     expect(worker_dict['working']).to_be_true()
+        expect(worker_dict['uuid']).to_equal(str(worker.uuid))
+        expect(worker_dict['last_ping']).to_equal(str(worker.last_ping))
+        expect(worker_dict['current_review']).to_equal(str(review.uuid))
+        expect(worker_dict['working']).to_be_true()
 
-    # def test_worker_is_working(self):
-    #     review = ReviewFactory.create()
-    #     worker = WorkerFactory.create()
-    #     worker2 = WorkerFactory.create(current_review=review)
+    def test_worker_is_working(self):
+        review = ReviewFactory.create()
+        worker = WorkerFactory.create()
+        worker2 = WorkerFactory.create(current_review=review)
 
-    #     expect(worker.working).to_be_false()
-    #     expect(worker2.working).to_be_true()
+        expect(worker.working).to_be_false()
+        expect(worker2.working).to_be_true()
 
     def test_can_get_worker_by_uuid(self):
         worker = WorkerFactory.create()
