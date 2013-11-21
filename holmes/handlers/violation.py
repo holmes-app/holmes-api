9599a24af7b8045fc4cdb00621ffc322c4d46edb
Rafael Floriano da Silva <rflorianobr@gmail.com>
2013-11-21 12:03:00 -0200

    Back with handler violation

---
+++ holmes/handlers/violation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from tornado import gen
+
+from holmes.handlers import BaseHandler
+from holmes.models import Violation
+
+
+class MostCommonViolationsHandler(BaseHandler):
+
+    @gen.coroutine
+    def get(self):
+        violations = Violation.get_most_common_violations(self.db)
+
+        result = []
+        for item in violations:
+            result.append({'name': item['title'], 'count': item['count']})
+
+        self.write_json(result)
+        self.finish()
