f7c1f0f917ff5cc0ebc750ebe59dce91215bb1c1
Rafael Floriano da Silva <rflorianobr@gmail.com>
2013-11-25 16:33:44 -0200

    Created sitemap validator

---
+++ holmes/validators/sitemap.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+
+from holmes.validators.base import Validator
+
+URL_RE = re.compile(
+    r'^(?:http|ftp)s?://'  # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+    r'localhost|'  # localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+    r'(?::\d+)?'  # optional port
+    r'(?P<relative>(?:/?|[/?]\S+))$', re.IGNORECASE)
+
+HTML_ENTITIES = re.compile(r'(&amp;|&apos;|&quot;|&gt;|&lt;)')
+INVALID_CHARS = re.compile(r'(&|\'|"|>|<)')
+
+
+class SitemapValidator(Validator):
+    MAX_SITEMAP_SIZE = 10  # 10 MB
+    MAX_LINKS_SITEMAP = 50000
+
+    def validate(self):
+        for sitemap, size in self.review.data['sitemap.files.size'].items():
+            response = self.review.data['sitemap.data'][sitemap]
+
+            if response.status_code > 399:
+                self.add_violation(
+                    key='sitemaps.not_found',
+                    title='Sitemaps not found',
+                    description='',
+                    points=100
+                )
+                return
+
+            if not response.text.strip():
+                self.add_violation(
+                    key='sitemaps.empty',
+                    title='Empty sitemaps file',
+                    description='',
+                    points=100
+                )
+                return
+
+            size_mb = (size / 1024.0)
+            urls_count = self.review.data['sitemap.files.urls'][sitemap]
+            not_encoded_links = 0
+
+            if size_mb > self.MAX_SITEMAP_SIZE:
+                self.add_violation(
+                    key='total.size.sitemap',
+                    title='Sitemap size in MB is too big.',
+                    description='There\'s %.2fMB of Sitemap in the %s file. '
+                                'Sitemap files should not exceed %s MB.'
+                                % (size_mb, sitemap, self.MAX_SITEMAP_SIZE),
+                    points=10
+                )
+
+            if urls_count > self.MAX_LINKS_SITEMAP:
+                self.add_violation(
+                    key='total.links.sitemap',
+                    title='Many links in a single sitemap.',
+                    description='There\'s %d links in the %s sitemap. '
+                                'Sitemap links should not exceed %s links.'
+                                % (urls_count, sitemap, self.MAX_LINKS_SITEMAP),
+                    points=10
+                )
+
+            for url in self.review.data['sitemap.urls'][sitemap]:
+                parse = URL_RE.match(url).groupdict()
+                relative = parse['relative']
+                encoded = True
+
+                if not relative:
+                    continue
+
+                try:
+                    str(relative).encode('utf-8')
+                except UnicodeDecodeError:
+                    encoded = False
+
+                relative = HTML_ENTITIES.sub('', relative)
+                encoded = encoded and not INVALID_CHARS.findall(relative)
+
+                if not encoded:
+                    not_encoded_links += 1
+
+            if not_encoded_links > 0:
+                self.add_violation(
+                    key='sitemaps.links.not_encoded',
+                    title='Url in sitemap is not encoded',
+                    description='There\'s %d not encoded links in the %s sitemap.'
+                                % (not_encoded_links, sitemap),
+                    points=10
+                )
---
+++ tests/unit/validators/test_sitemap.py
@@ -0,0 +1,232 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from mock import Mock
+from preggy import expect
+
+from holmes.config import Config
+from holmes.reviewer import Reviewer
+from holmes.validators.sitemap import SitemapValidator
+from tests.unit.base import ValidatorTestCase
+from tests.fixtures import PageFactory
+
+
+class TestSitemapValidator(ValidatorTestCase):
+
+    def test_add_violation_when_404(self):
+        page = PageFactory.create()
+
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            config=Config(),
+            validators=[]
+        )
+
+        validator = SitemapValidator(reviewer)
+        validator.review.data['sitemap.files.size'] = {'http://g1.globo.com/sitemap.xml': 10}
+        validator.review.data['sitemap.data'] = {'http://g1.globo.com/sitemap.xml': Mock(status_code=404, text=None)}
+        validator.add_violation = Mock()
+
+        validator.validate()
+
+        validator.add_violation.assert_called_once_with(
+            key='sitemaps.not_found',
+            title='Sitemaps not found',
+            description='',
+            points=100)
+
+    def test_add_violation_when_sitemap_is_empty(self):
+        page = PageFactory.create()
+
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            config=Config(),
+            validators=[]
+        )
+
+        validator = SitemapValidator(reviewer)
+        validator.review.data['sitemap.files.size'] = {'http://g1.globo.com/sitemap.xml': 10}
+        validator.review.data['sitemap.data'] = {'http://g1.globo.com/sitemap.xml': Mock(status_code=200, text='')}
+        validator.add_violation = Mock()
+
+        validator.validate()
+
+        validator.add_violation.assert_called_once_with(
+            key='sitemaps.empty',
+            title='Empty sitemaps file',
+            description='',
+            points=100)
+
+    def test_add_violation_when_sitemap_is_too_large(self):
+        page = PageFactory.create()
+
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            config=Config(),
+            validators=[]
+        )
+
+        validator = SitemapValidator(reviewer)
+        validator.review.data['sitemap.files.size'] = {'http://g1.globo.com/sitemap.xml': 10241}
+        validator.review.data['sitemap.data'] = {'http://g1.globo.com/sitemap.xml': Mock(status_code=200, text='data')}
+        validator.review.data['sitemap.files.urls'] = {'http://g1.globo.com/sitemap.xml': 10}
+        validator.review.data['sitemap.urls'] = {'http://g1.globo.com/sitemap.xml': []}
+        validator.add_violation = Mock()
+
+        validator.validate()
+
+        validator.add_violation.assert_called_once_with(
+            key='total.size.sitemap',
+            title='Sitemap size in MB is too big.',
+            description='There\'s 10.00MB of Sitemap in the http://g1.globo.com/sitemap.xml file. Sitemap files should not exceed 10 MB.',
+            points=10)
+
+    def test_add_violation_when_sitemap_has_many_links(self):
+        page = PageFactory.create()
+
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            config=Config(),
+            validators=[]
+        )
+
+        validator = SitemapValidator(reviewer)
+        validator.review.data['sitemap.files.size'] = {'http://g1.globo.com/sitemap.xml': 10}
+        validator.review.data['sitemap.data'] = {'http://g1.globo.com/sitemap.xml': Mock(status_code=200, text='data')}
+        validator.review.data['sitemap.files.urls'] = {'http://g1.globo.com/sitemap.xml': 50001}
+        validator.review.data['sitemap.urls'] = {'http://g1.globo.com/sitemap.xml': []}
+        validator.add_violation = Mock()
+
+        validator.validate()
+
+        validator.add_violation.assert_called_once_with(
+            key='total.links.sitemap',
+            title='Many links in a single sitemap.',
+            description='There\'s 50001 links in the http://g1.globo.com/sitemap.xml sitemap. Sitemap links should not exceed 50000 links.',
+            points=10)
+
+    def test_add_violation_when_sitemap_has_links_with_no_path(self):
+        page = PageFactory.create()
+
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            config=Config(),
+            validators=[]
+        )
+
+        validator = SitemapValidator(reviewer)
+        validator.review.data['sitemap.files.size'] = {'http://g1.globo.com/sitemap.xml': 10}
+        validator.review.data['sitemap.data'] = {'http://g1.globo.com/sitemap.xml': Mock(status_code=200, text='data')}
+        validator.review.data['sitemap.files.urls'] = {'http://g1.globo.com/sitemap.xml': 20}
+        validator.review.data['sitemap.urls'] = {'http://g1.globo.com/sitemap.xml': ['http://g1.globo.com/']}
+        validator.add_violation = Mock()
+
+        validator.validate()
+
+        expect(validator.add_violation.call_count).to_equal(0)
+
+    def test_add_violation_when_sitemap_has_links_that_not_need_to_be_encoded(self):
+        page = PageFactory.create()
+
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            config=Config(),
+            validators=[]
+        )
+
+        validator = SitemapValidator(reviewer)
+        validator.review.data['sitemap.files.size'] = {'http://g1.globo.com/sitemap.xml': 10}
+        validator.review.data['sitemap.data'] = {'http://g1.globo.com/sitemap.xml': Mock(status_code=200, text='data')}
+        validator.review.data['sitemap.files.urls'] = {'http://g1.globo.com/sitemap.xml': 20}
+        validator.review.data['sitemap.urls'] = {'http://g1.globo.com/sitemap.xml': ['http://g1.globo.com/1.html']}
+        validator.add_violation = Mock()
+
+        validator.validate()
+
+        expect(validator.add_violation.call_count).to_equal(0)
+
+    def test_add_violation_when_sitemap_has_links_that_need_to_be_encoded(self):
+        page = PageFactory.create()
+
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            config=Config(),
+            validators=[]
+        )
+
+        validator = SitemapValidator(reviewer)
+        validator.review.data['sitemap.files.size'] = {'http://g1.globo.com/sitemap.xml': 10}
+        validator.review.data['sitemap.data'] = {'http://g1.globo.com/sitemap.xml': Mock(status_code=200, text='data')}
+        validator.review.data['sitemap.files.urls'] = {'http://g1.globo.com/sitemap.xml': 20}
+        validator.review.data['sitemap.urls'] = {'http://g1.globo.com/sitemap.xml': ['http://g1.globo.com/ümlat.php']}
+        validator.add_violation = Mock()
+
+        validator.validate()
+
+        validator.add_violation.assert_called_once_with(
+            key='sitemaps.links.not_encoded',
+            title='Url in sitemap is not encoded',
+            description='There\'s 1 not encoded links in the http://g1.globo.com/sitemap.xml sitemap.',
+            points=10)
+
+    def test_add_violation_when_sitemap_has_links_that_need_to_be_encoded_with_amp(self):
+        page = PageFactory.create()
+
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            config=Config(),
+            validators=[]
+        )
+
+        validator = SitemapValidator(reviewer)
+        validator.review.data['sitemap.files.size'] = {'http://g1.globo.com/sitemap.xml': 10}
+        validator.review.data['sitemap.data'] = {'http://g1.globo.com/sitemap.xml': Mock(status_code=200, text='data')}
+        validator.review.data['sitemap.files.urls'] = {'http://g1.globo.com/sitemap.xml': 20}
+        validator.review.data['sitemap.urls'] = {'http://g1.globo.com/sitemap.xml': ['http://g1.globo.com/%C3%BCmlat.php&q=name']}
+        validator.add_violation = Mock()
+
+        validator.validate()
+
+        validator.add_violation.assert_called_once_with(
+            key='sitemaps.links.not_encoded',
+            title='Url in sitemap is not encoded',
+            description='There\'s 1 not encoded links in the http://g1.globo.com/sitemap.xml sitemap.',
+            points=10)
+
+    def test_add_violation_when_sitemap_with_good_link(self):
+        page = PageFactory.create()
+
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            config=Config(),
+            validators=[]
+        )
+
+        validator = SitemapValidator(reviewer)
+        validator.review.data['sitemap.files.size'] = {'http://g1.globo.com/sitemap.xml': 10}
+        validator.review.data['sitemap.data'] = {'http://g1.globo.com/sitemap.xml': Mock(status_code=200, text='data')}
+        validator.review.data['sitemap.files.urls'] = {'http://g1.globo.com/sitemap.xml': 20}
+        validator.review.data['sitemap.urls'] = {'http://g1.globo.com/sitemap.xml': ['http://g1.globo.com/%C3%BCmlat.php&amp;q=name']}
+        validator.add_violation = Mock()
+
+        validator.validate()
+
+        expect(validator.add_violation.call_count).to_equal(0)
