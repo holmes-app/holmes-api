9eaaf28391886c971ad7720999c46e0f09fa89d4
Marcelo Jorge Vieira <metal@alucinados.com>
2013-12-04 18:12:04 -0200

    SchemaOrgItemTypeValidator

--- holmes/validators/schema_org_item_type.py
+++ holmes/validators/schema_org_item_type.py
@@ -5,6 +5,42 @@ from holmes.validators.base import Validator
 
 
 class SchemaOrgItemTypeValidator(Validator):
+    @classmethod
+    def get_itemscope_message(cls, value):
+        return 'In order to conform to schema.org definition ' \
+               'of a webpage, the body tag must feature an ' \
+               'itemscope attribute.'
+
+    @classmethod
+    def get_itemtype_message(cls, value):
+        return 'In order to conform to schema.org definition ' \
+               'of a webpage, the body tag must feature an ' \
+               'itemtype attribute.'
+
+    @classmethod
+    def get_invalid_itemtype_message(cls, value):
+        url = 'http://schema.org/WebPage'
+        return 'In order to conform to schema.org definition ' \
+               'of a webpage, the body tag must feature an ' \
+               'itemtype attribute with a value of "%s" or ' \
+               'one of its more specific types.' % url
+
+    @classmethod
+    def get_violation_definitions(cls):
+        return {
+            'absent.schema.itemscope': {
+                'title': 'itemscope attribute missing in body',
+                'description': cls.get_itemscope_message
+            },
+            'absent.schema.itemtype': {
+                'title': 'itemtype attribute missing in body',
+                'description': cls.get_itemtype_message
+            },
+            'invalid.schema.itemtype': {
+                'title': 'itemtype attribute is invalid',
+                'description': cls.get_invalid_itemtype_message
+            }
+        }
 
     def validate(self):
         body = self.get_body()
@@ -20,33 +56,22 @@ class SchemaOrgItemTypeValidator(Validator):
             if 'itemscope' not in attributes:
                 self.add_violation(
                     key='absent.schema.itemscope',
-                    title='itemscope attribute missing in body',
-                    description='In order to conform to schema.org definition '
-                                'of a webpage, the body tag must feature an '
-                                'itemscope attribute.',
+                    value='',
                     points=10)
 
             has_itemtype = 'itemtype' in attributes
             if not has_itemtype:
                 self.add_violation(
                     key='absent.schema.itemtype',
-                    title='itemtype attribute missing in body',
-                    description='In order to conform to schema.org definition '
-                                'of a webpage, the body tag must feature an '
-                                'itemtype attribute.',
+                    value='',
                     points=10
                 )
 
             itemtype_value = self.reviewer.config.SCHEMA_ORG_ITEMTYPE
             if has_itemtype and body.get('itemtype') not in itemtype_value:
-                url = 'http://schema.org/WebPage'
                 self.add_violation(
                     key='invalid.schema.itemtype',
-                    title='itemtype attribute is invalid',
-                    description='In order to conform to schema.org definition '
-                                'of a webpage, the body tag must feature an '
-                                'itemtype attribute with a value of "%s" or '
-                                'one of its more specific types.' % (url),
+                    value='',
                     points=10
                 )
 
