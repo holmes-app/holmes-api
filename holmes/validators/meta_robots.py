b689bfb944cd2e83a8d686a5862f4c4af5916332
Bernardo Heynemann <heynemann@gmail.com>
2013-12-04 17:06:25 -0200

    Meta robots validator

--- holmes/validators/meta_robots.py
+++ holmes/validators/meta_robots.py
@@ -5,6 +5,26 @@ from holmes.validators.base import Validator
 
 
 class MetaRobotsValidator(Validator):
+    META_ROBOTS_NO_INDEX = 'A meta tag with the robots="noindex" ' \
+                           'attribute tells the search engines that ' \
+                           'they should not index this page.'
+
+    META_ROBOTS_NO_FOLLOW = 'A meta tag with the robots="nofollow" ' \
+                            'attribute tells the search engines that they ' \
+                            'should not follow any links in this page.'
+
+    @classmethod
+    def get_violation_definitions(cls):
+        return {
+            'presence.meta.robots.noindex': {
+                'title': 'Meta Robots with value of noindex.',
+                'description': lambda value: cls.META_ROBOTS_NO_INDEX
+            },
+            'presence.meta.robots.nofollow': {
+                'title': 'Meta Robots with value of nofollow.',
+                'description': lambda value: cls.META_ROBOTS_NO_FOLLOW
+            }
+        }
 
     def validate(self):
         tags = self.get_meta_tags()
@@ -13,20 +33,12 @@ class MetaRobotsValidator(Validator):
             if tag['key'] == 'robots' and tag['content'] == 'noindex':
                 self.add_violation(
                     key='presence.meta.robots.noindex',
-                    title='Meta Robots with value of noindex',
-                    description='A meta tag with the robots="noindex" '
-                                'attribute tells the search engines that '
-                                'they should not index this page.',
                     points=80
                 )
 
             if tag['key'] == 'robots' and tag['content'] == 'nofollow':
                 self.add_violation(
                     key='presence.meta.robots.nofollow',
-                    title='Meta Robots with value of nofollow',
-                    description='A meta tag with the robots="nofollow" '
-                                'attribute tells the search engines that they '
-                                'should not follow any links in this page.',
                     points=50
                 )
 
