3b491f3e0c84a08162024183ac2294702dec78a1
Marcelo Jorge Vieira <metal@alucinados.com>
2013-12-09 17:42:55 -0200

    Fixed TestFacts

--- tests/unit/models/test_facts.py
+++ tests/unit/models/test_facts.py
@@ -4,42 +4,44 @@
 from preggy import expect
 #from tornado.testing import gen_test
 
-from holmes.models import Fact
+from holmes.models import Fact, Key
 from tests.unit.base import ApiTestCase
 from tests.fixtures import FactFactory
 
 
 class TestFacts(ApiTestCase):
     def test_can_create_facts(self):
-        fact = FactFactory.create(value="whatever")
+        fact = FactFactory.create(
+            key=Key(name='some.random.fact'),
+            value='whatever'
+        )
 
         loaded_fact = self.db.query(Fact).get(fact.id)
 
-        expect(loaded_fact.key).to_equal(fact.key)
-        expect(loaded_fact.value).to_equal(fact.value)
-        expect(loaded_fact.unit).to_equal(fact.unit)
-        expect(loaded_fact.title).to_equal(fact.title)
+        expect(loaded_fact.key.name).to_equal('some.random.fact')
+        expect(loaded_fact.value).to_equal('whatever')
 
     def test_to_dict(self):
-        fact = FactFactory.build()
+        fact = FactFactory.create(
+            key=Key(name='some.random.fact'),
+            value='whatever',
+        )
 
-        expect(fact.to_dict()).to_be_like({
-            'title': fact.title,
-            'key': fact.key,
-            'unit': fact.unit,
-            'value': fact.value
+        facts_definitions = {'some.random.fact': {}}
+
+        expect(fact.to_dict(facts_definitions)).to_be_like({
+            'key': 'some.random.fact',
+            'value': 'whatever',
+            'unit': 'value',
+            'title': 'unknown'
         })
 
-    def test_fact_str_kb(self):
-        fact = FactFactory.create(key="some.random.fact", value=1203, title="title", unit="kb")
-
-        loaded_fact = self.db.query(Fact).get(fact.id)
-
-        expect(str(loaded_fact)).to_be_like('some.random.fact: 1203.00kb')
-
     def test_fact_str(self):
-        fact = FactFactory.create(key="some.random.fact", value=1203, title="title", unit="kms")
+        fact = FactFactory.create(
+            key=Key(name="some.random.fact"),
+            value=1203,
+        )
 
         loaded_fact = self.db.query(Fact).get(fact.id)
 
-        expect(str(loaded_fact)).to_be_like('some.random.fact: 1203kms')
+        expect(str(loaded_fact)).to_be_like('some.random.fact: 1203')
