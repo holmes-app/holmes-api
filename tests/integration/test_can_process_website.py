49e1c559dbee2902e85027625d94228e7f4bcd66
Marcelo Jorge Vieira <metal@alucinados.com>
2014-01-02 17:27:05 -0200

    Fixed Integration tests
    Thanks @heynemann

--- tests/integration/test_can_process_website.py
+++ tests/integration/test_can_process_website.py
@@ -2,46 +2,114 @@
 # -*- coding: utf-8 -*-
 
 from uuid import uuid4
-from mock import Mock
+import os
 
 from preggy import expect
+from octopus.model import Response
+from tests.fixtures import DomainFactory, PageFactory
+from tests.unit.base import ApiTestCase
+from mock import Mock
+import requests
 
 from holmes.reviewer import Reviewer
 from holmes.models.review import Review
 from holmes.models.page import Page
 from holmes.models.domain import Domain
 from holmes.config import Config
-from holmes.validators.total_requests import TotalRequestsValidator
-from holmes.validators.js_requests import JSRequestsValidator
-from holmes.validators.css_requests import CSSRequestsValidator
-from holmes.validators.link_crawler import LinkCrawlerValidator
-from holmes.validators.img_requests import ImageRequestsValidator
-from tests.fixtures import DomainFactory, PageFactory, ReviewFactory
-from tests.unit.base import ApiTestCase
+from holmes.worker import HolmesWorker
+from holmes.server import HolmesApiServer
 
 
-class CanProcessWebsiteTest(ApiTestCase):
-    #def setUp(self):
-        #super(CanProcessWebsiteTest, self).setUp()
-        #connect('holmes', host='localhost', port=6685, io_loop=self.io_loop)
+class CanProcessWebsiteTest(ApiTestCase, HolmesWorker):
+    def __init__(self, *args, **kw):
+        self.options = Mock(concurrency=10, verbose=0, cache=False)
+        ApiTestCase.__init__(self, *args, **kw)
+        self.initialize()
 
-    def get_config(self):
+    def async_get(self, url, handler, method='GET', **kw):
+        response = requests.get(url)
+        resp = Response(
+            url=url, status_code=response.status_code,
+            headers=dict([(key, value) for key, value in response.headers.items()]),
+            cookies=dict([(key, value) for key, value in response.cookies.items()]),
+            text=response.text, effective_url=response.url,
+            error=response.status_code > 399 and response.text or None,
+            request_time=response.elapsed and response.elapsed.total_seconds() or 0
+        )
+
+        handler(url, resp)
+
+    def get_server(self):
+        cfg = Config(**self.get_config())
+        debug = os.environ.get('DEBUG_TESTS', 'False').lower() == 'true'
+
+        self.server = HolmesApiServer(config=cfg, debug=debug, db=self.db)
+        return self.server
+
+    @property
+    def config(self):
         conf = dict(
             SQLALCHEMY_CONNECTION_STRING="mysql+mysqldb://root@localhost:3306/test_holmes",
             SQLALCHEMY_POOL_SIZE=20,
             SQLALCHEMY_POOL_MAX_OVERFLOW=10,
-            SQLALCHEMY_AUTO_FLUSH=True
+            SQLALCHEMY_AUTO_FLUSH=False,
+            COMMIT_ON_REQUEST_END=False,
+            REDISHOST='localhost',
+            REDISPORT=57575,
+            MATERIAL_GIRL_REDISHOST='localhost',
+            MATERIAL_GIRL_REDISPORT=57575,
+            FACTERS = [
+                'holmes.facters.title.TitleFacter',
+                'holmes.facters.links.LinkFacter',
+                #'holmes.facters.robots.RobotsFacter',
+                #'holmes.facters.sitemap.SitemapFacter',
+                #'holmes.facters.images.ImageFacter',
+                #'holmes.facters.css.CSSFacter',
+                'holmes.facters.meta_tags.MetaTagsFacter',
+                #'holmes.facters.js.JSFacter',
+                'holmes.facters.body.BodyFacter',
+                'holmes.facters.head.HeadFacter',
+                'holmes.facters.last_modified.LastModifiedFacter',
+                'holmes.facters.google_analytics.GoogleAnalyticsFacter',
+                'holmes.facters.heading_hierarchy.HeadingHierarchyFacter',
+            ],
+            VALIDATORS = [
+                'holmes.validators.title.TitleValidator',
+                #'holmes.validators.link_crawler.LinkCrawlerValidator',
+                #'holmes.validators.robots.RobotsValidator',
+                #'holmes.validators.sitemap.SitemapValidator',
+                #'holmes.validators.domain_canonicalization.DomainCanonicalizationValidator',
+                #'holmes.validators.img_requests.ImageRequestsValidator',
+                #'holmes.validators.css_requests.CSSRequestsValidator',
+                #'holmes.validators.js_requests.JSRequestsValidator',
+                #'holmes.validators.image_alt.ImageAltValidator',
+                'holmes.validators.anchor_without_any_text.AnchorWithoutAnyTextValidator',
+                'holmes.validators.meta_tags.MetaTagsValidator',
+                #'holmes.validators.meta_robots.MetaRobotsValidator',
+                'holmes.validators.required_meta_tags.RequiredMetaTagsValidator',
+                'holmes.validators.links_with_rel_nofollow.LinkWithRelNofollowValidator',
+                #'holmes.validators.link_with_redirect.LinkWithRedirectValidator',
+                'holmes.validators.schema_org_item_type.SchemaOrgItemTypeValidator',
+                'holmes.validators.body.BodyValidator',
+                'holmes.validators.link_with_rel_canonical.LinkWithRelCanonicalValidator',
+                'holmes.validators.blacklist.BlackListValidator',
+                'holmes.validators.open_graph.OpenGraphValidator',
+                'holmes.validators.last_modified.LastModifiedValidator',
+                'holmes.validators.google_analytics.GoogleAnalyticsValidator',
+                'holmes.validators.heading_hierarchy.HeadingHierarchyValidator',
+                'holmes.validators.url_with_underscore.UrlWithUnderscoreValidator',
+            ]
         )
         conf['MAX_JS_REQUESTS_PER_PAGE'] = 0
         conf['MAX_JS_KB_PER_PAGE_AFTER_GZIP'] = 0
         conf['MAX_CSS_REQUESTS_PER_PAGE'] = 0
         conf['MAX_CSS_KB_PER_PAGE_AFTER_GZIP'] = 0
 
-        return conf
+        return Config(**conf)
 
     def get_reviewer(
             self, api_url=None, page_uuid=None, page_url='http://page.url',
-            config=None, validators=None):
+            page_score=0.0, config=None):
 
         if api_url is None:
             api_url = self.get_url('/')
@@ -50,24 +118,26 @@ class CanProcessWebsiteTest(ApiTestCase):
             page_uuid = str(uuid4())
 
         if config is None:
-            config = Config(**self.get_config())
-
-        if validators is None:
-            validators = [
-                TotalRequestsValidator,
-                JSRequestsValidator,
-                CSSRequestsValidator,
-                LinkCrawlerValidator,
-                ImageRequestsValidator,
-            ]
+            config = self.config
 
         return Reviewer(
             api_url=api_url,
             page_uuid=str(page_uuid),
             page_url=page_url,
+            page_score=0.0,
             config=config,
-            validators=validators
+            validators=self.validators,
+            facters=self.facters,
+            wait=self.otto.wait,
+            wait_timeout=0,  # max time to wait for all requests to finish
+            db=self.db,
+            cache=self.cache,
+            publish=self.publish,
+            async_get=self.async_get,
+            fact_definitions=self.fact_definitions,
+            violation_definitions=self.violation_definitions,
         )
+
 
     def test_can_process_globo_com(self):
         self.db.query(Review).delete()
@@ -78,25 +148,19 @@ class CanProcessWebsiteTest(ApiTestCase):
 
         page = PageFactory.create(domain=domain, url='http://www.globo.com/')
 
-        review = ReviewFactory.create(page=page)
-
-        self.db.flush()
-
         reviewer = self.get_reviewer(
             api_url='http://localhost:2368',
             page_uuid=page.uuid,
             page_url='http://www.globo.com/'
         )
 
-        reviewer._wait_for_async_requests = Mock()
-
         reviewer.review()
 
-        Review.objects.get(review._id, callback=self.stop)
-        loaded_review = self.wait(timeout=30)
+        loaded_page = self.db.query(Page).get(page.id)
+        review = loaded_page.last_review
 
-        expect(len(loaded_review.facts)).to_be_greater_than(3)
-        expect(len(loaded_review.violations)).to_be_greater_than(3)
+        expect(len(review.facts)).to_be_greater_than(3)
+        expect(len(review.violations)).to_be_greater_than(3)
 
         print
         print
@@ -107,14 +171,14 @@ class CanProcessWebsiteTest(ApiTestCase):
         print
         print 'Facts'
         print '-----'
-        for fact in loaded_review.facts:
-            print str(fact)
+        for fact in review.facts:
+            print fact.to_dict(reviewer.fact_definitions)
 
         print
         print 'Violations'
         print '----------'
-        for violation in loaded_review.violations:
-            print str(violation)
+        for violation in review.violations:
+            print violation.to_dict(reviewer.violation_definitions)
 
         print
         print
