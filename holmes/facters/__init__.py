468515d0d0d60b9adc552b4a22086b3ce52c12db
Marcelo Jorge Vieira <metal@alucinados.com>
2014-01-14 18:51:17 -0200

    Refactoring Facter and Validator
    Creating Baser class

--- holmes/facters/__init__.py
+++ holmes/facters/__init__.py
@@ -4,15 +4,10 @@
 import urlparse
 
 
-class Facter(object):
-    unit = 'value'
+class Baser(object):
 
     def __init__(self, reviewer):
         self.reviewer = reviewer
-
-    @classmethod
-    def get_fact_definitions(cls):
-        raise NotImplementedError
 
     @property
     def page_uuid(self):
@@ -30,17 +25,11 @@ class Facter(object):
     def config(self):
         return self.reviewer.config
 
-    def get(self, url):
-        return self.reviewer._get(url)
+    def is_absolute(self, url):
+        return bool(urlparse.urlparse(url).scheme)
 
-    def add_fact(self, key, value):
-        self.reviewer.add_fact(key, value)
-
-    def add_violation(self, key, value):
-        self.reviewer.add_violation(key, value)
-
-    def async_get(self, url, handler, method='GET', **kw):
-        self.reviewer._async_get(url, handler, method, **kw)
+    def rebase(self, url):
+        return urlparse.urljoin(self.page_url.rstrip('/'), url)
 
     def is_valid(self, url):
         try:
@@ -48,11 +37,26 @@ class Facter(object):
         except ValueError:
             return None
 
-    def is_absolute(self, url):
-        return bool(urlparse.urlparse(url).scheme)
-
-    def rebase(self, url):
-        return urlparse.urljoin(self.page_url.rstrip('/'), url)
-
     def to_gzip(self, content):
         return content.encode('zip')
+
+    def add_fact(self, key, value):
+        self.reviewer.add_fact(key, value)
+
+
+class Facter(Baser):
+
+    unit = 'value'
+
+    @classmethod
+    def get_fact_definitions(cls):
+        raise NotImplementedError
+
+    def add_violation(self, key, value):
+        self.reviewer.add_violation(key, value)
+
+    def get(self, url):
+        return self.reviewer._get(url)
+
+    def async_get(self, url, handler, method='GET', **kw):
+        self.reviewer._async_get(url, handler, method, **kw)
--- holmes/validators/base.py
+++ holmes/validators/base.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import urlparse
+from holmes.facters import Baser
 
 
-class Validator(object):
+class Validator(Baser):
+
     def __init__(self, reviewer):
         self.reviewer = reviewer
         self.url_buffer = set()
@@ -13,48 +14,14 @@ class Validator(object):
     def get_violation_definitions(cls):
         raise NotImplementedError
 
-    def is_absolute(self, url):
-        return bool(urlparse.urlparse(url).scheme)
-
-    def rebase(self, url):
-        return urlparse.urljoin(self.page_url.rstrip('/'), url)
-
-    @property
-    def page_uuid(self):
-        return self.reviewer.page_uuid
-
-    @property
-    def page_url(self):
-        return self.reviewer.page_url
-
-    @property
-    def review(self):
-        return self.reviewer.review_dao
-
-    @property
-    def config(self):
-        return self.reviewer.config
+    def add_violation(self, key, value, points):
+        self.reviewer.add_violation(key, value, points)
 
     def validate(self):
         return True
 
-    def to_gzip(self, content):
-        return content.encode('zip')
-
     def enqueue(self, url):
         self.reviewer.enqueue(url)
-
-    def add_fact(self, key, value):
-        self.reviewer.add_fact(key, value)
-
-    def add_violation(self, key, value, points):
-        self.reviewer.add_violation(key, value, points)
-
-    def is_valid(self, url):
-        try:
-            return urlparse.urlparse(url)
-        except ValueError:
-            return None
 
     def test_url(self, url, response, broken_link_callback=None, moved_link_callback=None):
         status = response.status_code
@@ -89,7 +56,9 @@ class Validator(object):
         self.url_buffer = set()
 
     def broken_link_violation(self):
-        raise NotImplementedError('broken_link_violation method need to be implemented by {0}'.format(self.__class__.__name__))
+        text = 'broken_link_violation method need to be implemented by {0}'
+        raise NotImplementedError(text.format(self.__class__.__name__))
 
     def moved_link_violation(self):
-        raise NotImplementedError('moved_link_violation method need to be implemented by {0}'.format(self.__class__.__name__))
+        text = 'moved_link_violation method need to be implemented by {0}'
+        raise NotImplementedError(text.format(self.__class__.__name__))
