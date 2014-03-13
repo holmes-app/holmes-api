f3ab4144c7c15424f74ca05a9006e38c4623dc33
Marcelo Jorge Vieira <metal@alucinados.com>
2014-03-13 10:45:41 -0300

    GoogleAnalyticsFacter: Added unit

--- holmes/facters/google_analytics.py
+++ holmes/facters/google_analytics.py
@@ -14,6 +14,7 @@ class GoogleAnalyticsFacter(Facter):
             'page.google_analytics': {
                 'title': 'Google Analytics',
                 'description': lambda value: list(value),
+                'unit': 'google-analytics'
             }
         }
 
