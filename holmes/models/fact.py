3c200bd9b3469d51b0c49954b59e0448348ccc64
Marcelo Jorge Vieira <metal@alucinados.com>
2014-04-02 16:09:13 -0300

    Facts model: Removed dead code

--- holmes/models/fact.py
+++ holmes/models/fact.py
@@ -10,10 +10,8 @@ class Fact(Base):
     __tablename__ = "facts"
 
     id = sa.Column(sa.Integer, primary_key=True)
-    # key = sa.Column('key', sa.String(2000), nullable=False)
+
     value = sa.Column('value', JsonType, nullable=False)
-    #title = sa.Column('title', sa.String(2000), nullable=False)
-    #unit = sa.Column('unit', sa.String(2000), nullable=False)
 
     review_id = sa.Column('review_id', sa.Integer, sa.ForeignKey('reviews.id'))
     # review comes from Review relationship
