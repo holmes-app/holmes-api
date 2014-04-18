85050b1c1184ad32fb675f7deb6d624022db2158
Ricardo L. Dani <ricardodani@gmail.com>
2014-04-18 17:37:29 -0300

    Added Canonical Url's validator (Fix #94)

---
+++ holmes/validators/domain_canonicalization.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from holmes.utils import is_valid
+from holmes.validators.base import Validator
+
+
+class DomainCanonicalizationValidator(Validator):
+
+    @classmethod
+    def get_different_endpoints_description(cls, value):
+        return (
+            'This page have the canonical url`s "{}" and "{}" with '
+            'different endpoints. This both url`s should point '
+            'to the same location.'
+            'Not fixing this may cause search engines to be unsure as '
+            'to which URL is the correct one to index.'
+        ).format(value['no_www_url'], value['www_url'])
+
+    @classmethod
+    def get_no_301_description(cls, value):
+        return (
+            'This Canonical url "{}" is redirecting to "{}" with a different '
+            'than 301 status code: {}. This may cause search engines to be '
+            'unsure to where the URL is being redirected.'.format(
+                value.effective_url, value.headers['Location'],
+                value.status_code
+            )
+        )
+
+    @classmethod
+    def get_violation_definitions(cls):
+        return {
+            'page.canonicalization.different_endpoints': {
+                'title': 'Canonical URL`s have different endpoints',
+                'description': cls.get_different_endpoints_description,
+                'category': 'SEO'
+            },
+            'page.canonicalization.no_301_redirect': {
+                'title': 'Canonical URL`s have a non 301 redirect',
+                'description': cls.get_no_301_description,
+                'category': 'SEO'
+            }
+        }
+
+    def get_canonical_urls(self):
+        url = self.reviewer.page_url
+        parsed_url = is_valid(url)
+        scheme_url = parsed_url.scheme
+        domain_url = parsed_url.netloc
+
+        if domain_url.startswith('www.'):
+            www_url = url.rstrip('/')
+            no_www_url = '{}://{}'.format(scheme_url, domain_url[4:])
+        else:
+            no_www_url = url.rstrip('/')
+            www_url = '{}://www.{}'.format(scheme_url, domain_url)
+
+        return {'www_url': www_url, 'no_www_url': no_www_url}
+
+    def validate(self):
+        if not self.reviewer.is_root():
+            return
+
+        canonical_urls = self.get_canonical_urls()
+
+        self.async_get(canonical_urls['www_url'],
+                       self.handle_www_url_res,
+                       follow_redirects=False)
+        self.async_get(canonical_urls['no_www_url'],
+                       self.handle_no_www_url_res,
+                       follow_redirects=False)
+        self.reviewer.wait_for_async_requests()
+
+        if not self.has_same_effective_urls():
+            self.add_violation(
+                key='page.canonicalization.different_endpoints',
+                value=canonical_urls,
+                points=50
+            )
+        else:
+            redirect_error = self.has_redirect_violation()
+            if redirect_error:
+                self.add_violation(
+                    key='page.canonicalization.no_301_redirect',
+                    value=redirect_error,
+                    points=50
+                )
+
+    def handle_www_url_res(self, url, response):
+        self.www_url_res = response
+
+    def handle_no_www_url_res(self, url, response):
+        self.no_www_url_res = response
+
+    def has_same_effective_urls(self):
+        def target_url(res):
+            if res.status_code != 200 and 'Location' in res.headers:
+                return res.headers['Location'].rstrip('/')
+            else:
+                return res.effective_url.rstrip('/')
+
+        return target_url(self.www_url_res) == target_url(self.no_www_url_res)
+
+    def has_redirect_violation(self):
+        def test(res):
+            return res if (
+                'Location' in res.headers
+                and res.headers['Location'].rstrip('/')
+                    != res.effective_url.rstrip('/')
+                and res.status_code != 301
+            ) else None
+
+        return test(self.www_url_res) or test(self.no_www_url_res)
---
+++ tests/unit/validators/test_domain_canonicalization.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from mock import Mock, call
+from preggy import expect
+
+from holmes.config import Config
+from holmes.reviewer import Reviewer
+from holmes.validators.domain_canonicalization import (
+    DomainCanonicalizationValidator)
+from tests.unit.base import ValidatorTestCase
+from tests.fixtures import PageFactory
+
+
+class TestDomainCanonicalizationValidator(ValidatorTestCase):
+
+    @staticmethod
+    def get_validator_for_url(url=None):
+        page = PageFactory.create()
+        if url:
+            page.url = url
+        reviewer = Reviewer(
+            api_url='http://localhost:2368',
+            page_uuid=page.uuid,
+            page_url=page.url,
+            page_score=0.0,
+            config=Config(),
+            validators=[]
+        )
+        return DomainCanonicalizationValidator(reviewer)
+
+    def test_can_validate_page_url_not_root(self):
+        validator = self.get_validator_for_url('http://globo.com/abcd')
+        validator.async_get = Mock()
+        validator.get_canonical_urls = Mock()
+        expect(validator.validate()).to_equal(None)
+        expect(validator.async_get.called).to_be_false()
+        expect(validator.get_canonical_urls.called).to_be_false()
+
+    def test_get_canonical_urls_call(self):
+        url = 'http://globo.com/'
+        validator = self.get_validator_for_url(url)
+        expected = {'www_url': 'http://www.globo.com',
+                    'no_www_url': 'http://globo.com'}
+        expect(validator.get_canonical_urls()).to_equal(expected)
+        url = 'http://www.globo.com/'
+        validator = self.get_validator_for_url(url)
+        expected = {'www_url': 'http://www.globo.com',
+                    'no_www_url': 'http://globo.com'}
+        expect(validator.get_canonical_urls()).to_equal(expected)
+
+    def test_can_validate_async_calls(self):
+        url = 'http://globo.com/'
+        validator = self.get_validator_for_url(url)
+        validator.async_get = Mock()
+        validator.reviewer.wait_for_async_requests = Mock()
+        validator.has_same_effective_urls = Mock()
+        validator.has_redirect_violation = Mock()
+        validator.add_violation = Mock()
+        validator.validate()
+        expect(validator.async_get.call_count).to_equal(2)
+        expect(validator.async_get.call_args_list).to_include(
+            call('http://globo.com', validator.handle_no_www_url_res, follow_redirects=False)
+        )
+        expect(validator.async_get.call_args_list).to_include(
+            call('http://www.globo.com', validator.handle_www_url_res,
+                 follow_redirects=False)
+        )
+
+    def test_handle_async_get_response(self):
+        url = 'http://globo.com'
+        validator = self.get_validator_for_url(url)
+        expected_response = Mock(
+            status_code=200,
+            effective_url='http://globo.com/'
+        )
+        expected = ('http://globo.com', expected_response)
+        validator.handle_no_www_url_res(*expected)
+        expect(validator.no_www_url_res).to_equal(expected_response)
+        expected_response = Mock(
+            status_code=301,
+            effective_url='http://globo.com/',
+            headers={
+                'Location': 'http://www.globo.com/'
+            }
+        )
+        expected = ('http://www.globo.com', expected_response)
+        validator.handle_www_url_res(*expected)
+        expect(validator.www_url_res).to_equal(expected_response)
+
+    def test_has_same_effective_urls(self):
+        validator = self.get_validator_for_url()
+
+        validator.www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://www.globo.com/')
+        validator.no_www_url_res = Mock(
+            status_code=301, effective_url='http://globo.com/',
+            headers={'Location': 'http://www.globo.com/'})
+        expect(validator.has_same_effective_urls()).to_equal(True)
+
+        validator.www_url_res = Mock(
+            status_code=301, effective_url='http://www.globo.com/',
+            headers={'Location': 'http://globo.com/'})
+        validator.no_www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://globo.com/')
+        expect(validator.has_same_effective_urls()).to_equal(True)
+
+        validator.www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://www.globo.com/')
+        validator.no_www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://globo.com/')
+        expect(validator.has_same_effective_urls()).to_equal(False)
+
+    def test_has_redirect_error(self):
+        validator = self.get_validator_for_url()
+        validator.www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://www.globo.com/')
+        validator.no_www_url_res = Mock(
+            headers={'Location': 'http://www.globo.com/'},
+            status_code=302, effective_url='http://globo.com/')
+        expect(validator.has_redirect_violation()).to_equal(validator.no_www_url_res)
+
+        validator.www_url_res = Mock(
+            headers={'Location': 'http://globo.com/'},
+            status_code=302, effective_url='http://www.globo.com/')
+        validator.no_www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://globo.com/')
+        expect(validator.has_redirect_violation()).to_equal(validator.www_url_res)
+
+        validator.www_url_res = Mock(
+            headers={'Location': 'http://globo.com/'},
+            status_code=301, effective_url='http://www.globo.com/')
+        validator.no_www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://globo.com/')
+        expect(validator.has_redirect_violation()).to_equal(None)
+
+    def test_call_add_violation(self):
+        url = 'http://globo.com/'
+        validator = self.get_validator_for_url(url)
+        validator.www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://www.globo.com/')
+        validator.no_www_url_res = Mock(
+            status_code=301, effective_url='http://globo.com/',
+            headers={'Location': 'http://www.globo.com/'})
+        validator.async_get = Mock()
+        validator.reviewer.wait_for_async_requests = Mock()
+        validator.add_violation = Mock()
+        validator.validate()
+        expect(validator.add_violation.called).to_be_false()
+
+        validator.www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://www.globo.com/')
+        validator.no_www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://globo.com/')
+        validator.add_violation = Mock()
+        validator.validate()
+        expect(validator.add_violation.called).to_be_true()
+        validator.add_violation.assert_called_once_with(
+            key='page.canonicalization.different_endpoints',
+            value={
+                'www_url': 'http://www.globo.com',
+                'no_www_url': 'http://globo.com',
+            },
+            points=50
+        )
+
+        validator.www_url_res = Mock(
+            headers={},
+            status_code=200, effective_url='http://www.globo.com/')
+        validator.no_www_url_res = Mock(
+            headers={'Location': 'http://www.globo.com/'},
+            status_code=302, effective_url='http://globo.com/')
+        validator.add_violation = Mock()
+        validator.validate()
+        expect(validator.add_violation.called).to_be_true()
+        validator.add_violation.assert_called_once_with(
+            key='page.canonicalization.no_301_redirect',
+            value=validator.no_www_url_res,
+            points=50
+        )
+
+    def test_can_get_violation_definitions(self):
+        reviewer = Mock()
+        validator = DomainCanonicalizationValidator(reviewer)
+        definitions = validator.get_violation_definitions()
+
+        expect(definitions).to_length(2)
+        expect('page.canonicalization.different_endpoints'
+               in definitions).to_be_true()
+        expect('page.canonicalization.no_301_redirect'
+               in definitions).to_be_true()
+
+        expect(validator.get_different_endpoints_description({
+            'no_www_url': 'http://globo.com/',
+            'www_url': 'http://www.globo.com/'
+        })).to_equal(
+            'This page have the canonical url`s "http://globo.com/"'
+            ' and "http://www.globo.com/" with '
+            'different endpoints. This both url`s should point '
+            'to the same location.'
+            'Not fixing this may cause search engines to be unsure as '
+            'to which URL is the correct one to index.'
+        )
+
+        value = Mock(
+            headers={'Location': 'http://www.globo.com/'},
+            status_code=302, effective_url='http://globo.com/')
+        expect(validator.get_no_301_description(value)).to_equal(
+            'This Canonical url "http://globo.com/" is redirecting to '
+            '"http://www.globo.com/" with a different than 301 status code: 302. '
+            'This may cause search engines to be unsure to where the URL is '
+            'being redirected.'
+        )
