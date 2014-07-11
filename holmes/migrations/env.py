8ca9d7f1bbed6d47fa1f4a6b4e74d7e0d7bfa19b
Bernardo Heynemann <heynemann@gmail.com>
2014-07-11 14:17:44 -0300

    Removing useless comments

--- holmes/migrations/env.py
+++ holmes/migrations/env.py
@@ -5,25 +5,9 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from holmes import models
-# target_metadata = models.Base.metadata
 target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -43,34 +27,7 @@ def run_migrations_offline():
     with context.begin_transaction():
         context.run_migrations()
 
-
-#def run_migrations_online():
-    #"""Run migrations in 'online' mode.
-
-    #In this scenario we need to create an Engine
-    #and associate a connection with the context.
-
-    #"""
-    #engine = engine_from_config(
-                #config.get_section(config.config_ini_section),
-                #prefix='sqlalchemy.',
-                #poolclass=pool.NullPool)
-
-    #connection = engine.connect()
-    #context.configure(
-                #connection=connection,
-                #target_metadata=target_metadata
-                #)
-
-    #try:
-        #with context.begin_transaction():
-            #context.run_migrations()
-    #finally:
-        #connection.close()
-
-
 def run_migrations_online():
-    # Override sqlalchemy.url value to application's value
     alembic_config = config.get_section(config.config_ini_section)
 
     username = os.environ.get('MYSQL_USER', 'root')
